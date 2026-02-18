506ea770ccf24a72fd26036047156851ce7a7a65
Alex Horyshevskyi <oleksii120@gmail.com>
2026-02-18 12:09:53 +0200

    feat: vehicle ownership by region

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
+import polars as pl
+
 from src.data_load import load_data
-from src.analysis import most_common_vehicle_type
+from src.analysis import most_common_vehicle_type, vehicle_ownership_by_region
 
 
 def main():
@@ -11,10 +13,17 @@ def main():
     print("Data loaded successfully:")
     print(data_df.head())
 
-    print(f"Basic quality analysis:\n{data_df.describe()}")
+        
+    with pl.Config() as config:
+        config.set_tbl_rows(-1)
 
-    print("\nMost common vehicle types: ")
-    print(most_common_vehicle_type(data_df))
+        print(f"Basic quality analysis:\n{data_df.describe()}")
+
+        print("\nMost common vehicle types:")
+        print(most_common_vehicle_type(data_df))
+
+        print("\nVehicle ownership by region:")
+        print(vehicle_ownership_by_region(data_df))
 
 
 if __name__ == "__main__":
