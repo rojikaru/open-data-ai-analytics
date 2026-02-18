803b97a8150d033058a421f4424e09ddef73b5e3
Alex Horyshevskyi <oleksii120@gmail.com>
2026-02-18 12:09:53 +0200

    feat: vehicle ownership by region

--- src/analysis.py
+++ src/analysis.py
@@ -1,4 +1,5 @@
 import polars as pl
+from src.processing import map_koatuu_to_region
 
 
 def most_common_vehicle_type(
@@ -19,3 +20,38 @@ def most_common_vehicle_type(
         .sort("len", descending=True)[0]
         .row())
     return most_common
+
+
+def vehicle_ownership_by_region(
+        vehicles_df: pl.DataFrame,
+        target_codes: list[int] | None = None,
+        region_column: str = "REG_ADDR_KOATUU",
+        new_region_column: str = "HUMAN_REGION",
+        status_column: str = "OPER_CODE",
+) -> pl.DataFrame:
+    """
+    Analyze vehicle ownership by region.
+
+    :param vehicles_df (pl.DataFrame): A Polars DataFrame containing vehicle data.
+    :param region_column (str): The column name representing the region.
+    :param new_region_column (str): The column name for the human-readable region after mapping.
+    :param status_column (str): The column name representing the operational status.
+    :param target_codes (list[int]): The list of target operational codes to filter for.
+
+    :return (pl.DataFrame): A DataFrame showing the count of owned vs. non-owned vehicles by region.
+    """
+
+    if target_codes is None:
+        target_codes = [100, 105, 430]
+    
+    vehicle_acquiry_df = vehicles_df.filter(pl.col(status_column).is_in(target_codes))
+    transform_koatuu_to_human = (pl.col(region_column)
+        .map_elements(map_koatuu_to_region)
+        .alias(new_region_column))
+
+    ownership_by_region = (vehicle_acquiry_df
+        .with_columns(transform_koatuu_to_human)
+        .group_by([new_region_column])
+        .len()
+        .sort(new_region_column))
+    return ownership_by_region
---
+++ src/processing.py
@@ -0,0 +1,51 @@
+koatuu_mapping = {
+    1: "Autonomous Republic of Crimea",
+    5: "Vinnytsia Oblast",
+    7: "Volyn Oblast",
+    12: "Dnipro Oblast",
+    14: "Donetsk Oblast",
+    18: "Zhytomyr Oblast",
+    21: "Zakarpattia Oblast",
+    23: "Zaporizhzhia Oblast",
+    26: "Ivano-Frankivsk Oblast",
+    32: "Kyiv Oblast",
+    35: "Kirovohrad Oblast",
+    44: "Luhansk Oblast",
+    46: "Lviv Oblast",
+    48: "Mykolaiv Oblast",
+    51: "Odesa Oblast",
+    53: "Poltava Oblast",
+    56: "Rivne Oblast",
+    59: "Sumy Oblast",
+    61: "Ternopil Oblast",
+    63: "Kharkiv Oblast",
+    65: "Kherson Oblast",
+    68: "Khmelnytskyi Oblast",
+    71: "Cherkasy Oblast",
+    73: "Chernivtsi Oblast",
+    74: "Chernihiv Oblast",
+    80: "Kyiv",
+    85: "Sevastopol",
+}
+
+
+def map_koatuu_to_region(koatuu_code: int) -> str:
+    """
+    Map a KOATUU code to a human-readable region name.
+
+    :param koatuu_code (int): The KOATUU code to map.
+
+    :return (str): The corresponding region name.
+    """
+
+    koatuu_level_1 = str(koatuu_code)[:2]
+
+    first_pass = koatuu_mapping.get(int(koatuu_level_1), None)
+    if first_pass is not None:
+        return first_pass
+    
+    # Try to map using first digit only if 1, 5 or 7
+    if (koatuu_level_1[0] in {"1", "5", "7"}):
+        return koatuu_mapping.get(int(koatuu_level_1[0]), "Unknown Region")
+
+    return "Unknown Region"
