2db4d50501988e55e31e418029002c84db0636eb
Alex Horyshevskyi <oleksii120@gmail.com>
2026-02-18 11:28:21 +0200

    feat(data): minimalistic data quality analysis

--- src/main.py
+++ src/main.py
@@ -3,11 +3,14 @@ from src.data_load import load_data
 
 def main():
     data_df = load_data()
-    if data_df is not None:
-        print("Data loaded successfully:")
-        print(data_df.head())
-    else:
+    if data_df is None:
         print("Failed to load data.")
+        return
+    
+    print("Data loaded successfully:")
+    print(data_df.head())
+
+    print(f"Basic quality analysis:\n{data_df.describe()}")
 
 
 if __name__ == "__main__":
