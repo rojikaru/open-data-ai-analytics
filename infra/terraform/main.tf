a128affd7c2adbf445144fccf7b809a6453b1af9
Alex Horyshevskyi <oleksii120@gmail.com>
2026-04-29 22:50:30 +0300

    feat(infra): replace Azure Terraform with AWS EC2

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,119 +1,85 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
+provider "aws" {
+  region = var.aws_region
 }
 
-resource "azurerm_resource_group" "analytics" {
-  name     = "analytics-rg"
-  location = var.location
+resource "aws_key_pair" "analytics" {
+  key_name   = "analytics-key"
+  public_key = var.admin_ssh_public_key
 }
 
-resource "azurerm_virtual_network" "analytics" {
-  name                = "analytics-vnet"
-  location            = azurerm_resource_group.analytics.location
-  resource_group_name = azurerm_resource_group.analytics.name
-  address_space       = ["10.0.0.0/16"]
-}
+resource "aws_security_group" "analytics" {
+  name        = "analytics-sg"
+  description = "Allow SSH and web access"
 
-resource "azurerm_subnet" "analytics" {
-  name                 = "analytics-subnet"
-  resource_group_name  = azurerm_resource_group.analytics.name
-  virtual_network_name = azurerm_virtual_network.analytics.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "analytics" {
-  name                = "analytics-pip"
-  location            = azurerm_resource_group.analytics.location
-  resource_group_name = azurerm_resource_group.analytics.name
-  allocation_method   = "Static"
-}
-
-resource "azurerm_network_security_group" "analytics" {
-  name                = "analytics-nsg"
-  location            = azurerm_resource_group.analytics.location
-  resource_group_name = azurerm_resource_group.analytics.name
-
-  security_rule {
-    name                       = "allow-ssh"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  security_rule {
-    name                       = "allow-web"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = tostring(var.web_port)
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  ingress {
+    from_port   = var.web_port
+    to_port     = var.web_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "azurerm_network_interface" "analytics" {
-  name                = "analytics-nic"
-  location            = azurerm_resource_group.analytics.location
-  resource_group_name = azurerm_resource_group.analytics.name
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.analytics.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.analytics.id
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "analytics" {
-  network_interface_id      = azurerm_network_interface.analytics.id
-  network_security_group_id = azurerm_network_security_group.analytics.id
-}
+resource "aws_instance" "analytics" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.analytics.key_name
+  vpc_security_group_ids = [aws_security_group.analytics.id]
 
-resource "azurerm_linux_virtual_machine" "analytics" {
-  name                = "analytics-vm"
-  location            = azurerm_resource_group.analytics.location
-  resource_group_name = azurerm_resource_group.analytics.name
-  size                = var.vm_size
-  admin_username      = var.admin_username
-
-  network_interface_ids = [azurerm_network_interface.analytics.id]
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.admin_ssh_public_key
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku       = "server"
-    version   = "latest"
-  }
-
-  custom_data = base64encode(templatefile("${path.module}/cloud-init.yaml.tpl", {
+  user_data = templatefile("${path.module}/cloud-init.yaml.tpl", {
     repo_url       = var.repo_url
     admin_username = var.admin_username
     web_port       = var.web_port
-  }))
+  })
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "analytics-vm"
+  }
+}
+
+resource "aws_eip" "analytics" {
+  instance = aws_instance.analytics.id
+  domain   = "vpc"
 }
