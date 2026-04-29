6ccb6015b39b537e41a1e01d0ba72f63b8f33be7
Alex Horyshevskyi <oleksii120@gmail.com>
2026-04-29 23:16:01 +0300

    fix(aws): bigger instance

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3a.small"
+  default     = "t3a.large"
 }
 
 variable "admin_username" {
