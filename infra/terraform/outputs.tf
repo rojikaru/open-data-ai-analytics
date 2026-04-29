4640ef566e555a40d8b11ad58a3dbc16cd3aa1af
Alex Horyshevskyi <oleksii120@gmail.com>
2026-04-29 22:58:51 +0300

    chore(deploy): update outputs

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,9 +1,14 @@
 output "public_ip_address" {
   description = "Public IP address of the analytics VM"
-  value       = azurerm_public_ip.analytics.ip_address
+  value       = aws_eip.analytics.public_ip
 }
 
 output "app_url" {
   description = "URL of the web dashboard"
-  value       = "http://${azurerm_public_ip.analytics.ip_address}:${var.web_port}"
+  value       = "http://${aws_eip.analytics.public_ip}:${var.web_port}"
+}
+
+output "ssh_command" {
+  description = "SSH command to connect to the VM"
+  value       = "ssh ${var.admin_username}@${aws_eip.analytics.public_ip}"
 }
