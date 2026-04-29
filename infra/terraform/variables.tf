ef2903db023f7b08c9ad38bb2b1959f91da4a5de
Alex Horyshevskyi <oleksii120@gmail.com>
2026-04-29 22:37:51 +0300

    fix: set bigger vm variant

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -7,7 +7,7 @@ variable "location" {
 variable "vm_size" {
   description = "Azure VM SKU"
   type        = string
-  default     = "Standard_B2ats_v2"
+  default     = "Standard_B4ls_v2"
 }
 
 variable "admin_username" {
